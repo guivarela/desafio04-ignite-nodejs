5163227675a939c87066d94f0491dfb7eb76d665
guivarela <guivarela7@hotmail.com>
2021-03-20 13:23:41 -0300

    desafio finalizado, testes OK

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,13 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const userExists = this.usersRepository.findByEmail(email);
+
+    if (userExists) throw new Error("user already exists");
+
+    const user: User = this.usersRepository.create({ email, name });
+
+    return user;
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) throw new Error("user not found");
+
+    if (!user.admin) throw new Error("you must be admin to list all users");
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
